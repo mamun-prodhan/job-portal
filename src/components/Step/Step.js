43187709ea4a369432074bfc8917cdbb558e97ce
mamun-prodhan <mamunpr9@gmail.com>
2023-06-16 15:47:37 +0600

    steps completed with responsiveness

--- src/components/Step/Step.js
+++ src/components/Step/Step.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const Step = (props) => {
-    const {image, step, title,description} = props.step;
-    console.log(title);
-    return (
-        <div>
-            <h3>{step}</h3>
-            <img src={image} alt="arrow steps" />
-            <p>{title}</p>
-            <p>{description}</p>
-        </div>
-    );
+  const { image, step, title, description } = props.step;
+  console.log(title);
+  return (
+    <div className="bg-[#F8FAF7] rounded-lg p-6 lg:p-10">
+      <img src={image} alt="arrow steps" />
+      <h3 className="text-xl my-3">{step}</h3>
+      <p className="text-xl font-semibold mb-2">{title}</p>
+      <p>{description}</p>
+    </div>
+  );
 };
 
 export default Step;
--- src/components/Steps/Steps.js
+++ src/components/Steps/Steps.js
@@ -1,49 +1,49 @@
-import React from 'react';
-import img from '../../assets/icons/send.svg';
-import Step from '../Step/Step';
+import React from "react";
+import img from "../../assets/icons/send.svg";
+import Step from "../Step/Step";
 
 const Steps = () => {
+  const stepsData = [
+    {
+      image: img,
+      step: "STEP 1",
+      title: "Post your job",
+      description: "Enter your requirements for the job you need undertaken.",
+    },
+    {
+      image: img,
+      step: "STEP 2",
+      title: "Job is sent to trades",
+      description:
+        "Your job is sent out instantly to tradespeople local to you.",
+    },
+    {
+      image: img,
+      step: "STEP 3",
+      title: "Choose tradespeople",
+      description: "Choose the best tradespeople that fits your requirements. ",
+    },
+    {
+      image: img,
+      step: "STEP 4",
+      title: "Get instant responses",
+      description:
+        "Get responses from local tradespeople interested in undertaking your wokr ",
+    },
+  ];
 
-    const stepsData = [
-        {
-            image: img,
-            step: "STEP 1",
-            title: "Post your job",
-            description: "Enter your requirements for the job you need undertaken."
-        },
-        {
-            image: img,
-            step: "STEP 2",
-            title: "Job is sent to trades",
-            description: "Your job is sent out instantly to tradespeople local to you."
-        },
-        {
-            image: img,
-            step: "STEP 3",
-            title: "Choose tradespeople",
-            description: "Choose the best tradespeople that fits your requirements. "
-        },
-        {
-            image: img,
-            step: "STEP 4",
-            title: "Get instant responses",
-            description: "Get responses from local tradespeople interested in undertaking your wokr "
-        }
-    ]
-
-    return (
-        <div>
-            <h2 className='font-normal text-4xl text-center mt-32 mb-10'>Find a tradesperson have never been <span>so easy</span></h2>
-            <div className='grid grid-cols-4 gap-4'>
-                {
-                    stepsData.map((step, index) => <Step
-                    key = {index}
-                    step = {step}
-                    ></Step>)
-                }
-            </div>
-        </div>
-    );
+  return (
+    <div className="p-6 mb-12 lg:mb-24 lg:p-0">
+      <h2 className="font-normal text-2xl lg:text-4xl text-center mt-16 lg:mt-32 mb-5 lg:mb-10">
+        Find a tradesperson have never been <span>so easy</span>
+      </h2>
+      <div className="grid grid-cols-1 lg:grid-cols-4 gap-6 lg:gap-4">
+        {stepsData.map((step, index) => (
+          <Step key={index} step={step}></Step>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Steps;
