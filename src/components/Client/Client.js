527adaae8e253f84b607720886b808cfa18bf9b4
mamun-prodhan <mamunpr9@gmail.com>
2023-06-17 22:50:01 +0600

    mobile tablet desktop responsive

--- src/components/Client/Client.js
+++ src/components/Client/Client.js
@@ -3,9 +3,9 @@ import React from "react";
 const Client = (props) => {
   const { title, image } = props.client;
   return (
-    <div className="mr-2 lg:mr-3 text-[#FFFFFF] h-12 md:h-full p-2 lg:p-6 rounded-lg bg-[#3F8925] flex items-center justify-around">
+    <div className="mr-2 lg:mr-3 text-[#FFFFFF] h-12 md:h-16 lg:h-full p-2 md:p-6 lg:p-6 rounded-lg bg-[#3F8925] flex items-center justify-around">
       <div className="w-5/6">
-        <p className="text-xs md:text-4xl">{title}</p>
+        <p className="text-xs md:text-xl lg:text-4xl">{title}</p>
       </div>
       <div className="w-1/6">
         <img src={image} alt="icon" />
